52f64d65713b946835bdadafe03bf815968eb0c3
Tabor Wondimu <csmajortabor@gmail.com>
2022-02-23 22:36:02 -0800

    Finished Challenge set 3

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,48 +8,135 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            
+            if (vals == null || vals.Length == 0)
+            {
+                return false;
+            }
+            else if (vals.Contains(false))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
             throw new NotImplementedException();
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
+
+            if (numbers == null)
+            {
+                return false;
+            }
+            int num1 = numbers.Sum() % 2;
+            if (num1 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
             throw new NotImplementedException();
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
+           bool caseLower = false;
+           bool caseUpper = false;
+           bool isNumber = false;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLower(password[i]))
+                {
+                    caseLower = true;
+                }
+                if (char.IsUpper(password[i]))
+                {
+                    caseUpper = true;
+                }
+                if (char.IsNumber(password[i]))
+                {
+                    isNumber = true;
+                }
+            }
+                if (caseLower && caseUpper && isNumber)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
             throw new NotImplementedException();
         }
 
         public char GetFirstLetterOfString(string val)
         {
+            char letter = val[0];
+            return letter;
+
             throw new NotImplementedException();
         }
 
         public char GetLastLetterOfString(string val)
         {
+            char last = val[val.Length - 1];
+            return last;
             throw new NotImplementedException();
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
+            if (dividend == 0 || divisor == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return dividend / divisor;
+            }
+
             throw new NotImplementedException();
         }
 
         public int LastMinusFirst(int[] nums)
         {
+            int result = nums[nums.Length - 1] - nums[0];
+            return result;
+
             throw new NotImplementedException();
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
-        }
+            var list = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                if ( i% 2 != 0)
+                {
+                    list.Add(i);
+                }
+            }
+            return list.ToArray();
+
+                throw new NotImplementedException();
+            }
+           
+        
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+          for (var  i = 0; i < words.Length; i++)
+            {
+               words[i] = words[i].ToUpper();
+            }
         }
     }
 }
